b4c47d18fc3a9d5b4ed758dcc53d98e7ddb209aa
Croaker <249576@student.pwr.edu.pl>
2023-03-12 19:02:39 +0100

    msg

--- src/phobos_client.py
+++ src/phobos_client.py
@@ -1,9 +1,21 @@
 import socket
 
 host = socket.gethostbyname(socket.gethostname())
-port = 2137
+port = 9999
+listener  = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listener.connect((host, port))
 def phobos_listen():
-        listener  = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        listener.connect((host, port))
-        listener.send("Hello world !".encode('utf-8'))
-        print(listener.recv(4096))
+        done = False
+
+        while not done:
+                message = input("Message: ")
+                listener.send(message.encode('utf-8'))
+                incoming = listener.recv(4096).decode('utf-8')
+                if message == " ":
+                        listener.close()
+                        done = True
+                else:
+                        print(incoming)
+                #communication_socket.close())
+
+phobos_listen()
--- src/phobos_main.py
+++ src/phobos_main.py
@@ -1,3 +1,9 @@
 import phobos_client
 
+
+
+#while True:
+    #inp = input("press smth to attempt: ")
+    #if(inp != ""):
 phobos_client.phobos_listen()
+        #inp = ""
